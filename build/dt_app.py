ccf9ccd1725600e28d0235adde87fa9901d80809
Rafeeq Durowoju <59541303+walede@users.noreply.github.com>
2020-02-23 15:15:25 -0500

    Update dt_app.py

--- build/dt_app.py
+++ build/dt_app.py
@@ -599,6 +599,6 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
-mySL = streamer_sqlite.MyStreamListener(db.cnx, db.cur1, app)
+mySL = streamer.MyStreamListener(db.cnx, db.cur1, app)
 myStream = tweepy.Stream(auth=api.auth, listener=mySL)
 app.mainloop()
